6af68320c9d88dba5af48b252a00a8c0e58cc3e6
tianyichen <tchen94@jh.edu>
2024-09-05 19:40:03 -0400

    resolve the matrix version

--- graph_matching.r
+++ graph_matching.r
@@ -1,6 +1,11 @@
-pacman::p_load(segmented, igraph, irlba, locfit, tidyverse, doParallel, broom, vegan, Matrix)
+pacman::p_load(segmented, igraph, RSpectra, locfit, tidyverse, doParallel, broom, vegan, Matrix)
 library(igraph)
 library(iGraphMatch)
+
+#library('remotes')
+#install_version("Matrix", version='1.6-2' , INSTALL_opts = '--no-lock')
+
+
 registerDoParallel(detectCores()-1)
 set.seed(2)
 df.mds=NULL
@@ -26,7 +31,7 @@ D2 <- getD(df$Xhat)
 D2_shuffle <- getD(df$Xhat_shuffle) 
 D2_shuffle_GM <- getD(df$Xhat_shuffle_GM) 
 
-df.mds <- doMDS(D2,doplot = T)
+df.mds <- doMDS(D2,doplot = F)
 df.mds_shuffle <- doMDS(D2_shuffle,doplot = F)
 df.mds_shuffle_GM <- doMDS(D2_shuffle_GM,doplot = F)
 
@@ -34,12 +39,13 @@ mds <- df.mds$mds
 mds_shuffle <- df.mds_shuffle$mds
 mds_shuffle_GM <- df.mds_shuffle_GM$mds
 
-df.iso <- doIso(mds, mdsd=3)
-df.iso_shuffle <- doIso(mds_shuffle, mdsd=3)
-df.iso_shuffle_GM <- doIso(mds_shuffle_GM, mdsd=3)
+df.iso <- doIso(mds, mdsd=2)
+df.iso_shuffle <- doIso(mds_shuffle, mdsd=2)
+df.iso_shuffle_GM <- doIso(mds_shuffle_GM, mdsd=2)
 
 par(mfrow=c(1,3))
 plot(1:tmax,df.iso$iso)
 plot(1:tmax,df.iso_shuffle$iso)
-plot(1:tmax,df.iso_ shuffle_GM$iso)
+plot(1:tmax,df.iso_shuffle_GM$iso)
+
 
